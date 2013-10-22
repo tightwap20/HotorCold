117404095a695c0824815e45433844fdac89a4fc
tightwap20 <tightwap20@gmail.com>
2013-10-22 13:23:07 -0700

    changed name of html file to index and edited js css

--- hotorcold.js
+++ hotorcold.js
@@ -28,82 +28,95 @@ var rand = Math.floor(Math.random() * 100); //creates a random # from 1 to 100
 	var abs = Math.abs(diff);    //creates a positive number for the difference
 	console.log("this is the number " +rand); //
 
+
 	if (abs === 0) 
 	{
 		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 0px 20px #FC1501');
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 60px #FC1501');
 		$("p").replaceWith('<p>WooHoo! You guessed the number correctly</p>');
 		$("p").css('color','blue');
 	}
 	else if (abs >= 75 && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #3232ff');
 		$("p").replaceWith('<p>You are really cold and need to Guess Lower</p>');
 		$("p").css('color','blue');
 	}
 	else if (abs >= 50 && abs < 75 && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
 		$("p").replaceWith("<p>You are cold and need to Guess Lower</p>");
 		$("p").css('color','blue');
 	}
 	else if (abs >= 25 && abs < 50  && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
 		$("p").replaceWith("<p>You are warming up,but still need to Guess Lower</p>");
 		$("p").css('color','#CC3232');
 	}
 	else if (abs >= 10 && abs < 25  && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
 		$("p").replaceWith("<p>You are getting hot and need to guess lower</p>");
 		$("p").css('color','red');
 	}
 	else if (abs >= 5 && abs < 10  && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
 		$("p").replaceWith("<p>You are really hot and need to guess a little lower</p>");
 		$("p").css('color','red');
 	}
 	else if (abs < 5 && abs > 0 && val > rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
 		$("p").replaceWith("<p>You are on Fire and need to guess a little lower</p>");
 		$("p").css('color','#FC1501');
 	}
 	else if (abs >= 75 && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #3232ff');
 		$("p").replaceWith("<p>You are really cold and need to Guess Higher</p>");
 		$("p").css('color','blue');
 	}
 	else if (abs >= 50 && abs < 75 && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
 		$("p").replaceWith("<p>You are cold and need to Guess Higher</p>");
 		$("p").css('color', 'blue');
 	}
 	else if (abs >= 25 && abs < 50  && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
 		$("p").replaceWith("<p>You are warming up, but still need to Guess Higher</p>");
 		$("p").css('color','#CC3232');
 	}
 	else if (abs >= 10 && abs < 25  && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
 		$("p").replaceWith("<p>You are getting hot and need to guess Higher</p>");
 		$("p").css('color','red');
 	}
 	else if (abs >= 5 && abs < 10  && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
 		$("p").replaceWith("<p>You are really hot and need to guess a little Higher</p>");
 		$("p").css('color','red');
 	}
 	else if (abs < 5 && abs > 0 && val < rand) 
 	{
 		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
 		$("p").replaceWith("<p>You are on Fire and need to guess a little Higher</p>");
 		$("p").css('color','#FC1501');
 	}
