2b4480532aa7ea7ff32dc758d9ca2dc5f3fd3634
tightwap20 <tightwap20@gmail.com>
2013-12-05 10:27:28 -0700

    updated jquery

--- hotorcold.js
+++ hotorcold.js
@@ -1,123 +1,164 @@
-// $(document).ready(function() {
-//	var rand = Math.floor(Math.random() * 100);
-// 		$('#listInput').onKeydown(function() 
-//	{
-//	var textBox = document.getElementById('listInput');  //makes textbox equal to the input ID
-//	var val = textBox.value;     //grabs the text box value - input
-//	var diff = (val-rand);       //finds the difference between the random # and the value entered
-//	var abs = Math.abs(diff);    //creates a positive number for the difference
-//	console.log("this is the number " +rand); //
-//	}
-// }
 $(document).ready(function() 
 {
+
+/**** Global Variables ******/
 	$(".closeStatement").hide();
-});
+	$(".guesses").hide();
+	$(".totalGuesses").hide();
+	$(".resetButton").hide();
+	var rand = Math.floor(Math.random() * 100); //creates a random # from 1 to 100
+	var previousAbs; //creating a variable here to hold the value without changing within checkNumber
+	var abs;  //creating a variable here to hold the value without changing within checkNumber() keeps the old number
+ 	var answer = [];
 
-var rand = Math.floor(Math.random() * 100); //creates a random # from 1 to 100
+/***** Function that runs the checkNumber Function when enter is pressed *******/
+ $("#listInput").keypress(function(e) 
+ 	{
+      if(e.which == 13) 
+       {
+        checkNumber();
+        $(".guesses").show();       
+       }
+    });
 
-//$("#listInput").keypress(function(e) 
-	// {
- //    if(e.which == 13) 
- //    {
+/****** Input Function that takes the guess and spits out the response *******/
     function checkNumber() 
-	{
+		{
+	previousAbs = abs;   //storing the old value of abs, when new value entered this abs value is replaced by new abs value from line 24
 	var textBox = document.getElementById('listInput');  //makes textbox equal to the input ID
 	var val = textBox.value;     //grabs the text box value - input
 	var diff = (val-rand);       //finds the difference between the random # and the value entered
-	var abs = Math.abs(diff);    //creates a positive number for the difference
+	abs = Math.abs(diff);    //creates a positive number for the difference
 	console.log("this is the number " +rand); //
-
-
-	if (abs === 0) 
+	$("#listInput").val("");
+	$("#GuessNum").append(val + ",");
+	answer.push(val); //this pushes the value entered from the input into the var answer array
+		if (abs === 0) 
+		{
+			$(".closeStatement").fadeIn(100);
+			$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 60px #FC1501');
+			$("p").replaceWith('<p>WooHoo! You guessed the number correctly</p>');
+			$("p").css('color','red');
+			$("#totGuess").append(answer.length + " Guesses");
+			$(".totalGuesses").show();
+			$(".resetButton").show();
+		}
+		else if (val > rand)    //(abs >= 75 && val > rand) 
+		{
+			$(".closeStatement").fadeIn(100);
+			$("p").replaceWith('<p> You need to Guess Lower</p>');
+		}
+	// else if (abs >= 50 && abs < 75 && val > rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
+	// 	$("p").replaceWith("<p> need to Guess Lower</p>");
+	// 	$("p").css('color','blue');
+	// }
+	// else if (abs >= 25 && abs < 50  && val > rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
+	// 	$("p").replaceWith("<p> but still need to Guess Lower</p>");
+	// 	$("p").css('color','#CC3232');
+	// }
+	// else if (abs >= 10 && abs < 25  && val > rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
+	// 	$("p").replaceWith("<p> need to guess lower</p>");
+	// 	$("p").css('color','red');
+	// }
+	// else if (abs >= 5 && abs < 10  && val > rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
+	// 	$("p").replaceWith("<p> need to guess a little lower</p>");
+	// 	$("p").css('color','red');
+	// }
+	// else if (abs < 5 && abs > 0 && val > rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
+	// 	$("p").replaceWith("<p> need to guess a little lower</p>");
+	// 	$("p").css('color','#FC1501');
+	// }
+	else if (val < rand) //(abs >= 75 && val < rand) 
 	{
-		$(".closeStatement").fadeIn(1000);
+		$(".closeStatement").fadeIn(100);
+		//$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #3232ff');
+		$("p").replaceWith("<p> You need to Guess Higher</p>");
+		//$("p").css('color','blue');
+	}
+	// else if (abs >= 50 && abs < 75 && val < rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
+	// 	$("p").replaceWith("<p>need to Guess Higher</p>");
+	// 	$("p").css('color', 'blue');
+	// }
+	// else if (abs >= 25 && abs < 50  && val < rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
+	// 	$("p").replaceWith("<p> need to Guess Higher</p>");
+	// 	$("p").css('color','#CC3232');
+	// }
+	// else if (abs >= 10 && abs < 25  && val < rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
+	// 	$("p").replaceWith("<p> need to guess Higher</p>");
+	// 	$("p").css('color','red');
+	// }
+	// else if (abs >= 5 && abs < 10  && val < rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
+	// 	$("p").replaceWith("<p> need to guess a little Higher</p>");
+	// 	$("p").css('color','red');
+	// }
+	// else if (abs < 5 && abs > 0 && val < rand) 
+	// {
+	// 	$(".closeStatement").fadeIn(1000);
+	// 	$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
+	// 	$("p").replaceWith("<p> need to guess a little Higher</p>");
+	// 	$("p").css('color','#FC1501');
+	// }
+
+	if (previousAbs != null)  //if this is not the first guess do what is inside of it
+		{
+		if (previousAbs > abs && abs != 0)
+		{
+		$("p").prepend("Your getting hotter but ");
+		$("p").css('color','red');
 		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 60px #FC1501');
-		$("p").replaceWith('<p>WooHoo! You guessed the number correctly</p>');
+		}
+		else if (previousAbs < abs)
+		{
+		$("p").prepend("Your getting colder and ");
 		$("p").css('color','blue');
-	}
-	else if (abs >= 75 && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
 		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #3232ff');
-		$("p").replaceWith('<p>You are really cold and need to Guess Lower</p>');
-		$("p").css('color','blue');
+	 	}
+		else if (previousAbs == abs && abs != 0)
+		{
+		$("p").prepend("Your the same distance from the last guess ");
+		}
 	}
-	else if (abs >= 50 && abs < 75 && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
-		$("p").replaceWith("<p>You are cold and need to Guess Lower</p>");
-		$("p").css('color','blue');
-	}
-	else if (abs >= 25 && abs < 50  && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
-		$("p").replaceWith("<p>You are warming up,but still need to Guess Lower</p>");
-		$("p").css('color','#CC3232');
-	}
-	else if (abs >= 10 && abs < 25  && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
-		$("p").replaceWith("<p>You are getting hot and need to guess lower</p>");
-		$("p").css('color','red');
-	}
-	else if (abs >= 5 && abs < 10  && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
-		$("p").replaceWith("<p>You are really hot and need to guess a little lower</p>");
-		$("p").css('color','red');
-	}
-	else if (abs < 5 && abs > 0 && val > rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
-		$("p").replaceWith("<p>You are on Fire and need to guess a little lower</p>");
-		$("p").css('color','#FC1501');
-	}
-	else if (abs >= 75 && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #3232ff');
-		$("p").replaceWith("<p>You are really cold and need to Guess Higher</p>");
-		$("p").css('color','blue');
-	}
-	else if (abs >= 50 && abs < 75 && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #7f7fff');
-		$("p").replaceWith("<p>You are cold and need to Guess Higher</p>");
-		$("p").css('color', 'blue');
-	}
-	else if (abs >= 25 && abs < 50  && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ffa500');
-		$("p").replaceWith("<p>You are warming up, but still need to Guess Higher</p>");
-		$("p").css('color','#CC3232');
-	}
-	else if (abs >= 10 && abs < 25  && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff6666');
-		$("p").replaceWith("<p>You are getting hot and need to guess Higher</p>");
-		$("p").css('color','red');
-	}
-	else if (abs >= 5 && abs < 10  && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #ff3232');
-		$("p").replaceWith("<p>You are really hot and need to guess a little Higher</p>");
-		$("p").css('color','red');
-	}
-	else if (abs < 5 && abs > 0 && val < rand) 
-	{
-		$(".closeStatement").fadeIn(1000);
-		$(".closeStatement").css('-webkit-box-shadow' , '0px 20px 50px #FC1501');
-		$("p").replaceWith("<p>You are on Fire and need to guess a little Higher</p>");
-		$("p").css('color','#FC1501');
-	}
-};
+	};
+
+	//////* This Function starts the game over *////
+	$("#pageRefresh").click(function() {
+		$(".closeStatement").hide();
+		$(".guesses").hide();
+		$(".totalGuesses").hide();
+		$(".resetButton").hide();
+		$("#GuessNum").empty();
+		$("#totGuess").empty();
+		rand = Math.floor(Math.random() * 100); //
+		previousAbs = null; //
+		abs = null;  //
+ 		answer = [];
+
+	});
+});
